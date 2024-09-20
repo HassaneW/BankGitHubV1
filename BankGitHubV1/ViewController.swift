4874b03d84d3f2049951c08462c93ba5ce3e21d3
HassaneW <wandianga33@gmail.com>
2024-09-20 06:32:11 +0200

    Initial Commit

---
+++ BankGitHubV1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankGitHubV1
+//
+//  Created by 7MAKSACOD PC on 20/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
